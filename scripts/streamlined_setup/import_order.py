a499d6d546edd96a19e6e8ee4d5d47f161f74479
Michael <8632035+michael-gh1@users.noreply.github.com>
2022-08-02 22:20:03 -0700

    Moved join_body_parts into a separate component and disabled caching (did not work as intended)

--- scripts/streamlined_setup/import_order.py
+++ scripts/streamlined_setup/import_order.py
@@ -2,6 +2,12 @@
 
 import bpy
 
+try:
+    from join_body_parts_to_body import join_body_parts_to_body
+except:
+    pass
+
+
 FESTIVITY_ROOT_FOLDER_FILE_PATH = 'FESTIVITY_ROOT_FOLDER_FILE_PATH'
 CHARACTER_MODEL_FOLDER_FILE_PATH = 'CHARACTER_MODEL_FOLDER_FILE_PATH'
 BL_IDNAME_FUNCTION = 'function_to_call'
@@ -20,16 +26,21 @@ steps = {
         CACHE_KEY: ''
     },
     2: {
+        BL_IDNAME_FUNCTION: join_body_parts_to_body,
+        ENABLED: False,
+        CACHE_KEY: ''
+    },
+    3: {
         BL_IDNAME_FUNCTION: bpy.ops.file.genshin_import_model,
         ENABLED: True,
         CACHE_KEY: ''
     },
-    3: {
+    4: {
         BL_IDNAME_FUNCTION: bpy.ops.file.genshin_import_textures,
         ENABLED: True,
         CACHE_KEY: CHARACTER_MODEL_FOLDER_FILE_PATH
     },
-    4: {
+    5: {
         BL_IDNAME_FUNCTION: bpy.ops.file.genshin_import_material_data,
         ENABLED: True,
         CACHE_KEY: ''
@@ -38,28 +49,38 @@ steps = {
 
 
 def invoke_next_step(current_step_idx: int, file_path_to_cache=None):
-    if current_step_idx == 0 or current_step_idx > len(steps):
+    if current_step_idx <= 0 or current_step_idx > len(steps):
         return
-    if file_path_to_cache is not None:
-        cache_file_path(current_step_idx - 1, file_path_to_cache)
+    # Disabled cache, it does not work as intended b/c it carries over from past usage
+    # if file_path_to_cache is not None and steps[current_step_idx][CACHE_KEY]:
+    #     cache_file_path(current_step_idx - 1, file_path_to_cache)
     
     if steps[current_step_idx][ENABLED]:
-        execute_or_invoke = 'EXEC' if steps[current_step_idx][CACHE_KEY] else 'INVOKE'
         cached_file_directory = cache.get(steps[current_step_idx][CACHE_KEY], '')
+        execute_or_invoke = 'EXEC' if cached_file_directory else 'INVOKE'
         function_to_use = steps[current_step_idx][BL_IDNAME_FUNCTION]
 
-        print(f'Calling {function_to_use} with {execute_or_invoke}_DEFAULT')
-        function_to_use(
-                f'{execute_or_invoke}_DEFAULT', 
-                next_step_idx=current_step_idx + 1, 
-                file_directory=cached_file_directory
-        )
+        if type(function_to_use) is bpy.ops._BPyOpsSubModOp:
+            print(f'Calling {function_to_use} with {execute_or_invoke}_DEFAULT w/ cache: {cached_file_directory}')
+            function_to_use(
+                    f'{execute_or_invoke}_DEFAULT', 
+                    next_step_idx=current_step_idx + 1, 
+                    file_directory=cached_file_directory
+            )
+        else:
+            function_to_use(current_step_idx + 1)
     else:
         invoke_next_step(current_step_idx + 1)
 
 
 def cache_file_path(last_step_idx, file_path_to_cache):
-    if steps[last_step_idx][BL_IDNAME_FUNCTION] == steps[1][BL_IDNAME_FUNCTION]:
-        cache[FESTIVITY_ROOT_FOLDER_FILE_PATH] = file_path_to_cache
-    elif steps[last_step_idx][BL_IDNAME_FUNCTION] == steps[2][BL_IDNAME_FUNCTION] or steps[last_step_idx] == steps[3][BL_IDNAME_FUNCTION]:
-        cache[CHARACTER_MODEL_FOLDER_FILE_PATH] = file_path_to_cache
+    # if steps[last_step_idx][BL_IDNAME_FUNCTION] == steps[1][BL_IDNAME_FUNCTION]:
+    #     cache[FESTIVITY_ROOT_FOLDER_FILE_PATH] = file_path_to_cache
+    # elif steps[last_step_idx][BL_IDNAME_FUNCTION] == steps[2][BL_IDNAME_FUNCTION] or steps[last_step_idx] == steps[3][BL_IDNAME_FUNCTION]:
+    #     cache[CHARACTER_MODEL_FOLDER_FILE_PATH] = file_path_to_cache
+    
+    step = steps[last_step_idx]
+    step_cache_key = step.get(CACHE_KEY)
+
+    print(f'Assigning `{step_cache_key}:{file_path_to_cache}` in cache')
+    cache[step_cache_key] = file_path_to_cache
---
+++ scripts/streamlined_setup/join_body_parts_to_body.py
@@ -0,0 +1,31 @@
+import bpy
+
+try:
+    # from import_order import invoke_next_step
+    import import_order
+except:
+    print("ERROR: Couldn't import invoke_next_step, run the first step in setup_wizard")
+
+
+def join_body_parts_to_body(next_step_idx):
+    character_model = None
+    for object in bpy.context.scene.objects:
+        if object.type == 'ARMATURE':
+            character_model = object
+            continue
+
+    character_model_body = [body_part for body_part in character_model.children if body_part.name == 'Body'][0]
+    character_model_children = [body_part for body_part in character_model.children if body_part]
+    
+    for character_model_child in character_model_children:
+        print(f'Selecting {character_model_child} to join')
+        character_model_child.select_set(True)
+    bpy.context.view_layer.objects.active = character_model_body
+    print(f'Joining children body parts to {character_model_body}')
+    bpy.ops.object.join()
+
+    import_order.invoke_next_step(next_step_idx)
+
+
+if __name__ == '__main__':
+    join_body_parts_to_body(-1)
