8b0c69a5d1243748f6a17021d0a73c84edef3a99
Michael <8632035+michael-gh1@users.noreply.github.com>
2022-08-07 15:16:24 -0700

    fix: Deleting necessary empties (added a do not delete list)

--- scripts/setup_wizard/delete_empties.py
+++ scripts/setup_wizard/delete_empties.py
@@ -7,12 +7,21 @@ try:
 except:
     print("ERROR: Couldn't import invoke_next_step, but it's not needed if running this as a standalone")
 
+do_not_delete_object_list = [
+    'Camera',
+    'Face Light Direction',
+    'Head Driver',
+    'Hemi',
+    'Light',
+    'Main Light Direction'
+]
+
 
 def delete_empties(next_step_idx):
     scene = bpy.context.scene
 
     for object in scene.objects:
-        if object.type == 'EMPTY':
+        if object.type == 'EMPTY' and object.name not in do_not_delete_object_list:
             bpy.data.objects.remove(object)
 
     if next_step_idx:
